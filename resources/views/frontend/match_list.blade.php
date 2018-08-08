73c55eadffe0e9c2cd968965e2adc8805771ceb5
dansuorn.m <danusorn.m@tech-terrain.com>
2018-08-08 19:46:10 +0700

    update CSS matchPredict

--- resources/views/frontend/match_list.blade.php
+++ resources/views/frontend/match_list.blade.php
@@ -12,15 +12,15 @@
 		<div class="card-match border mt-4 p-2 pb-4">
 			<div class="row">
 				<div class="col-12">
-					<h3 class="match-page-title text-uppercase f-1">match of the weeks</h3>
-					<span class="f-4">23 Aug - 2 Sep</span>
+					<h3 class="match-page-title text-uppercase f-1">match of the weeks<br><span class="f-5 text-white">23 Aug - 2 Sep</span></h3>
+
 				</div>
 
 				<div class="col-12 dispay-match font-weight-bold mb-4">
 					<div class="d-flex justify-content-center">
 						<div class="align-self-start">
 							<img id="home-kits" class="kits px-4" src="{{ url('images/kits/sh_manchester_united.png') }}" />
-							<label class="home-kits-label float-left" for="home-kits">Manchester united</label>
+							<label class="home-kits-label f-1 float-left" for="home-kits">Manchester united</label>
 
 						</div>
 
@@ -29,7 +29,7 @@
 						</div>
 						<div class="align-self-start">
 							<img id="away-kits" class="kits px-4" src="{{ url('images/kits/sh_tottenhem.png') }}" />
-							<label for="away-kits float-right">Tottemham Hotspur</label>
+							<label class="f-1" for="away-kits">Tottemham Hotspur</label>
 						</div>
 
 					</div>
@@ -37,12 +37,12 @@
 				</div>
 				<div class="col-12">
 					<div class="d-flex justify-content-center">
-						<span class="times-remaining">เหลือเวลาอีก</span>
-						<div class="time-box rounded">33:20:01</div>
+						<span class="times-remaining mt-2 f-4">เหลือเวลาอีก</span>
+						<div class="time-box rounded f-1">38:20:01</div>
 					</div>
 				</div>
 				<div class="col-12">
-					<button type="submit" class="btn btn-green mt-4 btn-predict f-3">ทายผล<br><span>(22,112)</span></button>
+					<button type="submit" class="btn btn-green py-3 mt-4 btn-predict f-3">ทายผล<br><span class="f-5">(22,112)</span></button>
 				</div>
 
 			</div>
@@ -53,30 +53,28 @@
 				<h1 class="match-page-title-lastweek">MATCH <span>ของสัปดาห์ก่อน</span></h1>
 			</div>
 			<div class="col-12">
-				<div class="card-match-lastweek border p-4 mb-4">
+				<div class="card-match-lastweek border pt-2 px-4 mb-4">
 					<div class="row">
 						<div class="col-12">
 							<h3>
 								week 20 Aug - 26 Aug
 							</h3>
 						</div>
-						<div class="d-flex justify-content-between m-auto">
-							<div class="col-md-4 col-sm-4 text-center font-weight-bold mt-3">
-								<img class="kits" src="{{ url('images/kits/sh_Leicester_city.png') }}" />
-								<label for="" class="float-left w-100 away-team-home">Leicester City</label>
+						<div class="col-md-4 col-sm-4">
+							<img class="kits float-left" src="{{ url('images/kits/sh_Leicester_city.png') }}" />
+							<label for="" class="float-right f-3">Leicester City</label>
+						</div>
+						<div class="col-md-2 col-sm-2 text-center score">
+							3 - 2
+						</div>
+						<div class="col-md-4 col-sm-4">
+							<div class="w-100" >
+								<label for="" class="float-left f-3">Wolverhampton Wanderers</label>
 							</div>
-
-							<div class="col-md-3 col-sm-2 text-center score font-weight-bold mt-3">
-								3 - 2
-							</div>
-
-							<div class="col-md-4 col-sm-4 text-center font-weight-bold mt-3">
-								<img class="kits" src="{{ url('images/kits/sh_wolverhampton.png') }}" />
-								<label for="" class="float-left w-100 away-team-name">Wolverhampton Wanderers</label>
-							</div>
+							<img class="kits float-right" src="{{ url('images/kits/sh_wolverhampton.png') }}" />
 						</div>
 
-						<div class="col-md-3 col-sm-12  text-center">
+						<div class="col-md-2 col-sm-12  text-center">
 							<div class="total-predict">
 								<div class="bg-secondary">
 									<h4 class="mb-0  pt-1">ทายผลทั้งหมด</h4>
@@ -148,16 +146,16 @@
 						</div>
 
 						<div class="d-flex justify-content-between m-auto">
-							<div class="col-md-4 col-sm-4 text-center font-weight-bold mt-3">
+							<div class="col-md-4 col-sm-4 text-center mt-3">
 								<img class="kits" src="{{ url('images/kits/sh_arsenal.png') }}" />
-								<label for="" class="float-left w-100 away-team-home">Arsenal</label>
+								<label for="" class="float-left w-100 away-team-home f-4">Arsenal</label>
 							</div>
 
 							<div class="col-md-3 col-sm-2 text-center score font-weight-bold mt-3">
 								9 - 0
 							</div>
 
-							<div class="col-md-4 col-sm-4 text-center font-weight-bold mt-3">
+							<div class="col-md-4 col-sm-4 text-center mt-3">
 								<img class="kits" src="{{ url('images/kits/sh_manchester_city.png') }}" />
 								<label for="" class="float-left w-100 away-team-name">Manchester City</label>
 							</div>
