95d2b1d21ba13aa45124ce48603ce0a0bfdd21d7
Papassorn Phopong <spriten.papassorn@gmail.com>
2018-08-09 22:02:14 +0700

    fix bug

--- resources/views/emails/ForgotPassword.php
+++ resources/views/emails/ForgotPassword.php
@@ -1,1 +1,2 @@
-<p>test email</p>
+<p style="text-align: left;"><strong>Cigna Football Match Of The Weeks</strong></p>
+<p style="text-align: left;">Click link to <a href="{{$url}}">Reset Password</a></p>
