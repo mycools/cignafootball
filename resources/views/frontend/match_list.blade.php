a9ac270decc91bc6b8422c35e0f4c8fafb4f807a
SANTICHAI  SAKSOONG <santichai.saksoong@gmail.com>
2018-08-10 04:45:35 +0700

    Fix count

--- resources/views/frontend/match_list.blade.php
+++ resources/views/frontend/match_list.blade.php
@@ -7,16 +7,17 @@
 @section('content')
 <link rel="stylesheet" type="text/css" href="/css/match.css" />
 @php
-if(strtotime($matchInfo['bet_start']) <= strtotime('now')) {
+$now = Carbon\Carbon::now();
+$now = $now->toDateTimeString();
 
-	$time = $matchInfo['bet_end'];
-} else if(strtotime($matchInfo['bet_end']) >= strtotime('now')) {
+if(strtotime($matchInfo['match_start']) <= strtotime($now) && strtotime($matchInfo['match_end']) >= strtotime($now)) {
 
-	$time = $matchInfo['bet_end'];
+	$time = $matchInfo['match_end'];
 } else {
 
 	$time = "";
 }
+
 @endphp
 <div class="match-page">
 	<div class="container">
