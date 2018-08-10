cb690dcec264e91602c4ba887a851746f90abc9a
AJENAJEN <i_hear_nie@hotmail.com>
2018-08-10 12:41:13 +0700

    revise text copy

--- resources/views/frontend/components/home-share.blade.php
+++ resources/views/frontend/components/home-share.blade.php
@@ -3,7 +3,7 @@
 		<div class="box-bg-img">
 			<div class="content color-white">
 				แชร์กิจกรรม <span class="text-large">Match Of The Weeks</span><br />
-				ทายผลฟุตบอลอังกฤษ <span class="color-yellow font-italic">ลุ้นรับรางวัลมูลค่า 30 ล้านบาท</span>
+				ทายผลฟุตบอลอังกฤษ <span class="color-yellow font-italic">ลุ้นรางวัลดูบอลถึงอังกฤษทุกเดือน</span>
 				<div class="row mt-15">
 					<div class="col-8 col-sm-6 col-md-4 m-auto">
 						<a class="btn btn-white color-blue w-100 pt-10 pb-10 btnShare" id="sharefb" href="javascript:;"><i class="fa fa-share-alt mgr-5"></i> คลิกแชร์กิจกรรม</a>
