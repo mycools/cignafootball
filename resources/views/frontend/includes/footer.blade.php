1835d0687eab7f2b795913fe30311a5b3fcb7d72
AJENAJEN <i_hear_nie@hotmail.com>
2018-08-27 22:36:41 +0700

    no message

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -4,7 +4,7 @@
 					Copyright © 2018 Match Of The Weeks
 				</div>
 				<div class="col-12 col-md-auto mb-3 mb-md-0 text-center text-md-right">
-					Power by 
+					Powered by 
 					<img style="height: 30px;" src="{{ url('images/logo/woody_world.png') }}" />
 				</div>
 		</footer>
