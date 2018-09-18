84e83ad9248d9f5b10f9dcf47d470d373d016913
Arnut Jiamrattanapitak <tle417@gmail.com>
2018-09-18 21:31:28 +0700

    update model

--- app/Models/UserIncomplete.php
+++ app/Models/UserIncomplete.php
@@ -27,4 +27,19 @@ class UserIncomplete extends Authenticatable
         'team_id',
         'tc_accept'
     ];
+
+    public function routeNotificationForCorpSMS()
+    {
+        return $this->phoneno;
+    }
+
+    public function getRank()
+    {
+        return $this->hasOne('App\Models\Ranks', 'user_id', 'id');
+    }
+
+    public function pointlogs()
+    {
+        return $this->morphMany('App\Models\PointLogs', 'taggable');
+    }
 }
