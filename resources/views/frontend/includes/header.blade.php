8b8d2b45038f0e5805bc5c9abf9df8b5741104f2
Arnut Jiamrattanapitak <tle417@gmail.com>
2018-10-29 11:20:18 +0700

    menu thai lang

--- resources/views/frontend/includes/header.blade.php
+++ resources/views/frontend/includes/header.blade.php
@@ -60,29 +60,29 @@
 	                <div class="navbar-collapse collapse w-100" id="navbarNavDropdown">
     					<ul class="navbar-nav w-100 justify-content-center nav-pills nav-fill">
 				            <li class="nav-item">
-								<a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ url('/') }}">Home</a>
+								<a class="nav-link {{ Request::is('/') ? 'active' : '' }}" href="{{ url('/') }}">หน้าแรก</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link {{ Request::is('match') ? 'active' : '' }}" href="{{ url('match') }}">Match</a>
+								<a class="nav-link {{ Request::is('match') ? 'active' : '' }}" href="{{ url('match') }}">ทายผล</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link {{ Request::is('ranking') ? 'active' : '' }}" href="{{ url('ranking') }}">Ranking</a>
+								<a class="nav-link {{ Request::is('ranking') ? 'active' : '' }}" href="{{ url('ranking') }}">อันดับผู้เล่น</a>
 							</li>
 							<!-- <li class="nav-item">
 								<a class="nav-link {{ Request::is('healthtips') ? 'active' : '' }}" href="{{ url('healthtips') }}">Health Tips</a>
 							</li> -->
 							<li class="nav-item">
-								<a class="nav-link {{ Request::is('prize') ? 'active' : '' }}" href="{{ url('prize') }}">Prize</a>
+								<a class="nav-link {{ Request::is('prize') ? 'active' : '' }}" href="{{ url('prize') }}">รางวัล</a>
 							</li>
 							<li class="nav-item">
-								<a class="nav-link {{ Request::is('rules') ? 'active' : '' }}" href="{{ url('rules') }}">Rules</a>
+								<a class="nav-link {{ Request::is('rules') ? 'active' : '' }}" href="{{ url('rules') }}">เงื่อนไข</a>
 							</li>
 						</ul>
 						<ul class="nav navbar-nav ml-auto w-25 justify-content-end">
 
 							@if(!Auth::user())
 							<li class="nav-item pdl-5">
-								<a class="nav-link pr-0 special {{ Request::is('signin') ? 'active' : '' }}" href="{{ url('signin') }}"><img src="{{ url('images/icon/icon_invite_menu.png') }}" /> Sign in</a>
+								<a class="nav-link pr-0 special {{ Request::is('signin') ? 'active' : '' }}" href="{{ url('signin') }}"><img src="{{ url('images/icon/icon_invite_menu.png') }}" /> เข้าสู่ระบบ</a>
 							</li>
 							@endif
 
@@ -95,15 +95,15 @@
 							<li class="nav-item dropdown d-none d-md-block">
 								<a class="nav-link pr-0 special dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img src="{{ url('images/icon/icon_profile_menu.png') }}" /></a>
 								<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-			                        <a class="dropdown-item {{ Request::is('profile') ? 'active' : '' }}" href="{{ url('profile') }}">Profile</a>
-			                        <a class="dropdown-item last-child" href="{{ url('signout') }}">Sign Out</a>
+			                        <a class="dropdown-item {{ Request::is('profile') ? 'active' : '' }}" href="{{ url('profile') }}">ข้อมูลส่วนตัว</a>
+			                        <a class="dropdown-item last-child" href="{{ url('signout') }}">ออกจากระบบ</a>
 			                    </div>
 							</li>
 							<li class="nav-item d-block d-md-none">
-								<a class="nav-link special {{ Request::is('profile') ? 'active' : '' }}" href="{{ url('profile') }}"><span class="d-flex d-md-none">Profile</span><img src="{{ url('images/icon/icon_profile_menu.png') }}" /></a>
+								<a class="nav-link special {{ Request::is('profile') ? 'active' : '' }}" href="{{ url('profile') }}"><span class="d-flex d-md-none">ข้อมูลส่วนตัว</span><img src="{{ url('images/icon/icon_profile_menu.png') }}" /></a>
 							</li>
 							<li class="nav-item d-block d-md-none last-child">
-								<a class="nav-link special color-blue" href="{{ url('signout') }}"><span class="d-flex d-md-none">Sign Out</span></a>
+								<a class="nav-link special color-blue" href="{{ url('signout') }}"><span class="d-flex d-md-none">ออกจากระบบ</span></a>
 							</li>
 							@endif
 
