c5b235f216cfd528b31c40bac3805dd029703f92
Arnut Jiamrattanapitak <tle417@gmail.com>
2018-12-14 16:12:18 +0700

    add embed facebook

--- resources/views/frontend/components/home-share.blade.php
+++ resources/views/frontend/components/home-share.blade.php
@@ -1,7 +1,13 @@
 <div class="row justify-content-center mt-45 mt-sm-30 pb-30 pb-sm-30 padding-mobile">
 	<div class="col-12 mb-sm-15 p-sm-0 d-none d-sm-block">
 		<div class="box-bg-img">
+		<div class="row justify-content-center">
+			<div class="col-12 col-sm-4 mb-sm-15 p-sm-0 d-none d-sm-block">
+				<div class="fb-page" data-href="https://www.facebook.com/matchoftheweeks" data-width="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+			</div>
+			<div class="col-12 col-sm-8 mb-sm-15 p-sm-0 d-none d-sm-block">
 			<div class="content color-white">
+			
 				แชร์กิจกรรม <span class="text-large">Match Of The Weeks</span><br />
 				ทายผลฟุตบอลอังกฤษ <span class="color-yellow font-italic">ลุ้นรางวัลดูบอลถึงอังกฤษทุกเดือน</span>
 				<div class="row mt-15">
@@ -10,11 +16,24 @@
 					</div>
 				</div>
 			</div>
+			</div>
+		</div>
 		</div>
 	</div>
 	<div class="col-10 col-sm-8 col-sm-3 m-auto d-block d-sm-none">
+		<div class="fb-page" data-href="https://www.facebook.com/matchoftheweeks" data-width="300" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"></div>
+	</div>
+	<div class="col-10 col-sm-8 col-sm-3 m-auto d-block d-sm-none" style="padding-top:20px">
 		<a class="btn btn-blue color-white w-100 pt-10 pb-10 btnShare" id="sharefbmb" href="javascript:;">
 			<i class="fa fa-share-alt mgr-5"></i> คลิกแชร์กิจกรรม
 		</a>
 	</div>
 </div>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/th_TH/sdk.js#xfbml=1&version=v3.2&appId=220285388098453&autoLogAppEvents=1';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
